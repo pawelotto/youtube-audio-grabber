83d6cbe61dc0a594e626a01f190b9a0d5dab6c4f
Paweł Otto <po@wurk.pl>
2017-12-26 10:45:43 +0100

    Final commit for release v1.0

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import fn from './functions'
+import * as assert from 'assert'
+
+const url = process.argv[2]
+const skip = parseInt(process.argv[3]) || 0
+assert.ok(url, 'Please provide valid youtube url for video')
+
+main()
+
+async function main(){
+  const formatInfo = await fn.getAvailableFormats(url)
+  const highest = fn.getHighestBitrateFormat(formatInfo.formats)
+  if(highest) { 
+    const file = await fn.grabFile(formatInfo.title, url, highest)
+    fn.convertFile(file, skip)
+  }
+}
